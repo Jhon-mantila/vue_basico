8dbfdaddde087cb9aa6fcafa013aa0c34a52e663
Jhon-mantila <kaiser_4tu@hotmail.com>
2023-12-05 17:12:24 -0500

    Fin curso

---
+++ index.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>Lorem  ipsum</p>
+    <p>{{ text }}</p>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                text: "hola jhon vue"
+            }
+        }
+    }
+</script>
+<style>
+    p{
+        color: blue;
+    }
+</style>
